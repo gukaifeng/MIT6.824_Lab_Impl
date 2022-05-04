3d9e0f609fd7fbdc5f381e091ad9c546a3649764
gukaifeng <github@gukaifeng.me>
2022-05-05 01:06:38 +0800

    passed the 3A test steadily, but there were performance issues with the last 6 tests in the 3A

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -16,6 +16,22 @@ func produceClientId() int {
 	return kvClientId
 }
 
+type RPCState struct {
+	state   bool
+	muState sync.Mutex
+}
+
+func (s *RPCState) setRPCState(ok bool) {
+	s.muState.Lock()
+	defer s.muState.Unlock()
+	s.state = ok
+}
+func (s *RPCState) getRPCState() bool {
+	s.muState.Lock()
+	defer s.muState.Unlock()
+	return s.state
+}
+
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
@@ -36,7 +52,7 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
-	ck.prevLeader = -1
+	ck.prevLeader = int(nrand()) % len(ck.servers)
 	ck.clientId = produceClientId()
 	ck.nextRequestSeq = 1
 	return ck
@@ -58,8 +74,8 @@ func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
 
-	// ck.muOperate.Lock()
-	// defer ck.muOperate.Unlock()
+	ck.muOperate.Lock()
+	defer ck.muOperate.Unlock()
 
 	args := &GetArgs{key, ck.clientId, ck.nextRequestSeq}
 
@@ -71,17 +87,18 @@ func (ck *Clerk) Get(key string) string {
 		}
 
 		// ok := ck.servers[ck.prevLeader].Call("KVServer.Get", args, reply)
-		var ok bool
+		// var ok bool
+		s := &RPCState{}
 		t0 := time.Now()
-		go ck.sendGet(ck.prevLeader, args, reply, &ok)
+		go ck.sendGet(ck.prevLeader, args, reply, s)
 
-		for time.Since(t0).Milliseconds() < 100 {
-			if ok {
+		for time.Since(t0).Milliseconds() < 280 {
+			if s.getRPCState() {
 				break
 			}
 		}
 
-		if !ok || reply.Err == ErrWrongLeader {
+		if !s.getRPCState() || reply.Err == ErrWrongLeader {
 			ck.prevLeader = -1
 		} else if reply.Err == OK {
 			ck.nextRequestSeq++
@@ -107,32 +124,40 @@ func (ck *Clerk) Get(key string) string {
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 
 	// You will have to modify this function.
-	// ck.muOperate.Lock()
-	// defer ck.muOperate.Unlock()
+
+	ck.muOperate.Lock()
+	defer ck.muOperate.Unlock()
 
 	args := &PutAppendArgs{key, value, op, ck.clientId, ck.nextRequestSeq}
 
-	for {
-		DPrintf("客户端尝试 k = %v, v = %v, op = %v", key, value, op)
+	for i := 1; true; i++ {
+		DPrintf("客户端 %v 开始第 %v 次尝试 k = %v, v = %v, op = %v，访问的服务器是 %v",
+			ck.clientId, i, key, value, args, ck.prevLeader)
 		reply := &PutAppendReply{}
-		if ck.prevLeader == -1 {
-			ck.prevLeader = int(nrand()) % len(ck.servers)
-		}
-
-		// ok := ck.servers[ck.prevLeader].Call("KVServer.PutAppend", args, reply)
-		var ok bool
+		s := &RPCState{}
 		t0 := time.Now()
-		go ck.sendPutAppend(ck.prevLeader, args, reply, &ok)
+		go ck.sendPutAppend(ck.prevLeader, args, reply, s)
 
-		for time.Since(t0).Milliseconds() < 100 {
-			if ok {
+		for time.Since(t0).Milliseconds() < 280 {
+			if s.getRPCState() {
 				break
 			}
 		}
 
-		if !ok || reply.Err == ErrWrongLeader {
-			ck.prevLeader = -1
+		if !s.getRPCState() {
+			DPrintf("客户端 %v 第 %v 次尝试 k = %v, v = %v, op = %v 失败，访问的服务器是 %v, 原因是 RPC 超时未返回",
+				ck.clientId, i, key, value, args, ck.prevLeader)
+			ck.prevLeader = (ck.prevLeader + 1) % len(ck.servers)
+			continue
+		}
+
+		if reply.Err == ErrWrongLeader {
+			DPrintf("客户端 %v 第 %v 次尝试 k = %v, v = %v, op = %v 失败，访问的服务器是 %v, 原因是访问的服务器不是领导者",
+				ck.clientId, i, key, value, args, ck.prevLeader)
+			ck.prevLeader = (ck.prevLeader + 1) % len(ck.servers)
 		} else if reply.Err == OK {
+			DPrintf("客户端 %v 第 %v 次尝试 k = %v, v = %v, op = %v 成功，访问的服务器是 %v",
+				ck.clientId, i, key, value, args, ck.prevLeader)
 			ck.nextRequestSeq++
 			break
 		}
@@ -146,10 +171,13 @@ func (ck *Clerk) Append(key string, value string) {
 	ck.PutAppend(key, value, "Append")
 }
 
-func (ck *Clerk) sendGet(server int, args *GetArgs, reply *GetReply, ok *bool) {
-	*ok = ck.servers[server].Call("KVServer.Get", args, reply)
+func (ck *Clerk) sendGet(server int, args *GetArgs, reply *GetReply, s *RPCState) {
+	ok := ck.servers[server].Call("KVServer.Get", args, reply)
+	s.setRPCState(ok)
+
 }
 
-func (ck *Clerk) sendPutAppend(server int, args *PutAppendArgs, reply *PutAppendReply, ok *bool) {
-	*ok = ck.servers[server].Call("KVServer.PutAppend", args, reply)
+func (ck *Clerk) sendPutAppend(server int, args *PutAppendArgs, reply *PutAppendReply, s *RPCState) {
+	ok := ck.servers[server].Call("KVServer.PutAppend", args, reply)
+	s.setRPCState(ok)
 }
